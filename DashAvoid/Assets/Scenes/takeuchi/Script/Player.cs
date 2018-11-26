585208a2b117a3aca4a840862209f1965d07c584
takeuchidaina <yda.takeuchidaina@gmail.com>
2018-11-26 17:20:12 +0900

    [add] Playerの画像、スクリプトの追加と左右移動
    画像はただの四角です。
    プレイヤーが左右に動きます、左は現状移動できてしまいますので今後速度減少にしていきます。

---
+++ DashAvoid/Assets/Scenes/takeuchi/Script/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove();
+	}
+
+    //プレイヤーの移動 
+    void PlayerMove()
+    {
+        //→
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.3f, 0, 0);
+        }
+        //←(移動速度減少予定)
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(0.3f, 0, 0);
+        }
+    }
+}
+
+
+
