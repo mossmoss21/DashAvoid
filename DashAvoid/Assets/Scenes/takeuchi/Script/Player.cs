2a45aa03db792ad44815235e0279e3c34ed1ef5d
takeuchidaina <yda.takeuchidaina@gmail.com>
2018-11-30 16:38:37 +0900

    [update] プレイヤーの基盤の作成
    想定している関数を追加しました。
    ボス時のフラグも追加しております。

--- DashAvoid/Assets/Scenes/takeuchi/Script/Player.cs
+++ DashAvoid/Assets/Scenes/takeuchi/Script/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private int isBossFlg;
+
+    // Use this for initialization
+    void Start () {
+        int isBossFlg = 0;
 	}
 	
 	// Update is called once per frame
@@ -14,21 +16,63 @@ public class Player : MonoBehaviour {
         PlayerMove();
 	}
 
-    //プレイヤーの移動 
-    void PlayerMove()
-    {
-        //→
-        if (Input.GetKey(KeyCode.RightArrow))
+    // 移動 
+    void PlayerMove(){
+
+        // 通常時の動き
+        if(isBossFlg == 0)
         {
-            transform.position += new Vector3(0.3f, 0, 0);
+            //→
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(0.3f, 0, 0);
+            }
+            //←(移動速度減少予定)
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position -= new Vector3(0.3f, 0, 0);
+            }
         }
-        //←(移動速度減少予定)
-        if (Input.GetKey(KeyCode.LeftArrow))
+        // ボス時の動き
+        else
         {
-            transform.position -= new Vector3(0.3f, 0, 0);
+            //→
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(0.3f, 0, 0);
+            }
+            //←
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position -= new Vector3(0.3f, 0, 0);
+            }
         }
+
+
     }
+
+    // 攻撃
+    void PlayerAttack(){
+        //Xキー
+        //敵を攻撃
+        //弾を消せる
+    }
+
+    // ジャンプ
+    void Jump(){
+        //スペースキー or Zキー
+        //押してる長さでジャンプ力変化
+        //三段階か四段階でポイントがあって
+        //そこの時点でSpaceキーが押されているか判断
+        //離されていたら落下(RigidBody)
+
+    }
+
+    // フラッシュ
+    void Frash(){
+        //Cキー
+        //押したときにフラッシュ可動範囲ならフラッシュアクション
+        //switchでオブジェクトによって動作を分ける
+    }
+
 }
-
-
-
