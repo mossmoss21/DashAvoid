136019c5ad083b8617488648d29cb3651fe495ce
Mellmag <yda.tm.2360623@gmail.com>
2019-01-08 12:30:05 +0900

    [add] Bullet追加画像

---
+++ DashAvoid/Assets/Scenes/mizuno/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 3.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ DashAvoid/Assets/Scenes/mizuno/Bullet_C.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_C : MonoBehaviour {
+
+    [SerializeField]
+    public int LRFlag ;
+
+
+    // Use this for initialization
+    void Start () {
+        
+        GetComponent<Rigidbody2D>().velocity = new Vector3(0, 6.0f , 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+       GetComponent<Rigidbody2D>().velocity = Quaternion.Euler(0f, 0f, 0.3f * LRFlag) * GetComponent<Rigidbody2D>().velocity;
+
+    }
+}
