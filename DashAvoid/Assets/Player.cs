113a14b35eaa3eb95f32af96ae7c66ad46d91a0b
ysaito <ysaito.yda@gmail.com>
2019-01-08 12:34:50 +0900

    {add}mapの追加

---
+++ DashAvoid/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.5f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 300.0f, 0));
+        }
+
+    }
+}
