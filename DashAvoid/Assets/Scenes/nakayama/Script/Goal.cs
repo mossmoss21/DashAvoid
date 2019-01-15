dca465d24872a80bffe08d5d50e51ef09245dcc8
GM2180136\nwuser <mizuki060411@gmail.com>
2019-01-15 16:57:07 +0900

    [add]  ゴールできるようにした
    中山瑞基

---
+++ DashAvoid/Assets/Scenes/nakayama/Script/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+
+    //カウントダウンのテキストの表示の関数
+    [SerializeField]
+    private Text _textCountdown;
+
+    // Use this for initialization
+    void Start () {
+        //テキストを非表示にする
+        _textCountdown.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //接触したら"ゴール"
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        _textCountdown.gameObject.SetActive(true);      //カウントのテキストが表示する
+        _textCountdown.text = "GOOOOOOL";
+    }
+
+}
