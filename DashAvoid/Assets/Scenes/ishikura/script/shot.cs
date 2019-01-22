807c376515e4376ff315f90f2e10c1cca0a14bde
takasiro <41959070+takasiro@users.noreply.github.com>
2019-01-22 15:23:06 +0900

    コミット

---
+++ DashAvoid/Assets/Scenes/ishikura/script/shot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shot : MonoBehaviour {
+    GameObject target;
+    Vector3 velocity;
+
+    float rad;
+    float deg;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("player");
+        velocity= Vector3.zero;
+        rad = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dx =target.transform.position.x - transform.position.x;
+            float dy = target.transform.position.y - transform.position.y;
+        rad =  Mathf.Atan2(dy,dx);
+        deg = rad * Mathf.Rad2Deg;
+        transform.position += Vector3.MoveTowards(transform.position,target.transform.position,1f);
+	}
+}
---
+++ DashAvoid/Assets/Scenes/taki/script/item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);   //自分を消去する
+    }
+}
