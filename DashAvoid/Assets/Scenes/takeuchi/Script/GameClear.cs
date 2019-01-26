cbb9f5f4f842abe9801541ccab954c1e87e302e2
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-01-26 19:47:03 +0900

    [add]GameClearのシーンの作成
    ゴールの透明な当たり判定に触れるとシーンがクリアに変わります

---
+++ DashAvoid/Assets/Scenes/takeuchi/Script/GameClear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //当たったらシーンをGAMECLEARへ
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("GameClear");
+    }
+}
