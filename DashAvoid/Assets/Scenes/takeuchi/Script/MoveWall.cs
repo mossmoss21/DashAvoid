aff5cea77880aa305e41b489819afb596dec41fb
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-01-26 19:49:08 +0900

    [add]迫りくる壁の追加
    当たると死にます

---
+++ DashAvoid/Assets/Scenes/takeuchi/Script/MoveWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //移動
+        transform.position += new Vector3(0.005f, 0f, 0);
+
+    }
+
+    //当たったらシーンをGAMEOVERへ
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+}
