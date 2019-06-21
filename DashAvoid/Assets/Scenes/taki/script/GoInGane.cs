b4927b96711110e273600c1ee921647b20b22eef
takasiro <takagiyda@gmail.com>
2019-06-21 15:21:59 +0900

    [update] シーン移動用のscript追加
    ボタンををしたらそのシーンに飛べるように、scriptを追加しました

---
+++ DashAvoid/Assets/Scenes/taki/script/GoInGane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoInGane : MonoBehaviour {
+
+    [SerializeField] Graphics turebottom;
+
+
+
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+}
